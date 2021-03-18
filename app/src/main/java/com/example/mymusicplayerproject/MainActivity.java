4e4b40b0ac13e72f8250483e235bb43e2c26d611
mingle <mingle0729@naver.com>
2021-03-18 17:40:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mymusicplayerproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mymusicplayerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("MUSIC PLAYER");
+    }
+}
